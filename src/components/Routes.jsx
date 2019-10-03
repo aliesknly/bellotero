3a4282006e11149ba5775fc1fa9fc89df15b0450
kanly <aliesky.perez@gmail.com>
2019-10-03 15:44:10 -0400

    Redirect + ScreenShots

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,15 +3,17 @@ import {Switch, Route} from 'react-router-dom';
 //Components
 import Page1 from './pages/Page1';
 import Configurator from './pages/Configurator';
+import Home from "./pages/Home";
 
 
 const Routes = () =>{
     return(
         <Switch>
-            <Route path="/page-1" component={Page1}/>
-            <Route path="/page-2" component={Configurator}/>
+            <Route path={"/"} exact component={Home}/>
+            <Route path="/page-1" exact component={Page1}/>
+            <Route path="/page-2" exact component={Configurator}/>
         </Switch>
     )
-}
+};
 
 export default Routes
