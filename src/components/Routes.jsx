694d17098cb686a106f1a6e612ff34bbeaa6c1d6
kanly <aliesky.perez@gmail.com>
2019-10-03 15:50:54 -0400

    Add Routes

---
+++ src/components/Routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom';
+//Components
+import Page1 from './pages/Page1';
+import Configurator from './pages/Configurator';
+import Home from "./pages/Home";
+
+
+const Routes = () =>{
+    return(
+        <Switch>
+            <Route path={"/"} exact component={Home}/>
+            <Route path="/page-1" exact component={Page1}/>
+            <Route path="/page-2" exact component={Configurator}/>
+        </Switch>
+    )
+};
+
+export default Routes
